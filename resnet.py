ee3cb68635dc8dc840a686a6e8f05dab0bd7f228
Reric Young <Reric@outlook.com>
2022-03-18 15:34:10 +0800

    Add ResNet50

--- resnet.py
+++ resnet.py
@@ -3,7 +3,7 @@ import torch.nn.functional as f
 
 
 class ResBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride):
+    def __init__(self, in_channels, out_channels, stride=1):
         super(ResBlock, self).__init__()
         self.block = nn.Sequential(
             nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3,
@@ -28,10 +28,38 @@ class ResBlock(nn.Module):
         return y
 
 
+class BottleNeck(nn.Module):
+    def __init__(self, in_channels, mid_channels, out_channels, stride=1):
+        super(BottleNeck, self).__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=mid_channels, kernel_size=1, stride=1, bias=False),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=mid_channels, out_channels=mid_channels, kernel_size=3, stride=stride, padding=1,
+                      bias=False),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=mid_channels, out_channels=out_channels, kernel_size=1, stride=1, bias=False),
+            nn.BatchNorm2d(out_channels)
+        )
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1,
+                          stride=stride, bias=False)
+            )
+
+    def forward(self, x):
+        y = self.block(x)
+        y += self.shortcut(x)
+        y = f.relu(y, inplace=True)
+        return y
+
+
 class ResNet34(nn.Module):
     def __init__(self, num_class):
         super(ResNet34, self).__init__()
-        self.conv1 = nn.Sequential(
+        self.conv = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7,
                       stride=2, padding=3, bias=False),
             # size = /2
@@ -41,31 +69,84 @@ class ResNet34(nn.Module):
             # size = /4
         )
         self.block = nn.Sequential(
-            ResBlock(64, 64, 1),
-            ResBlock(64, 64, 1),
-            ResBlock(64, 64, 1),
+            ResBlock(64, 64),
+            ResBlock(64, 64),
+            ResBlock(64, 64),
+            # conv2_x
             ResBlock(64, 128, 2),
             # size = /8
-            ResBlock(128, 128, 1),
-            ResBlock(128, 128, 1),
-            ResBlock(128, 128, 1),
+            ResBlock(128, 128),
+            ResBlock(128, 128),
+            ResBlock(128, 128),
+            # conv3_x
             ResBlock(128, 256, 2),
             # size = /16
-            ResBlock(256, 256, 1),
-            ResBlock(256, 256, 1),
-            ResBlock(256, 256, 1),
-            ResBlock(256, 256, 1),
-            ResBlock(256, 256, 1),
+            ResBlock(256, 256),
+            ResBlock(256, 256),
+            ResBlock(256, 256),
+            ResBlock(256, 256),
+            ResBlock(256, 256),
+            # conv4_x
             ResBlock(256, 512, 2),
             # size = /32
-            ResBlock(512, 512, 1),
-            ResBlock(512, 512, 1),
+            ResBlock(512, 512),
+            ResBlock(512, 512),
+            # conv5_x
             nn.AvgPool2d(kernel_size=8)
         )
         self.fc = nn.Linear(512, num_class)
 
     def forward(self, x):
-        x = self.conv1(x)
+        x = self.conv(x)
+        x = self.block(x)
+        x = x.view(x.size()[0], -1)
+        x = self.fc(x)
+        return x
+
+
+class ResNet50(nn.Module):
+    def __init__(self, num_class):
+        super(ResNet50, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7,
+                      stride=2, padding=3, bias=False),
+            # size = /2
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+            # size = /4
+        )
+        self.block = nn.Sequential(
+            BottleNeck(64, 64, 256),
+            BottleNeck(256, 64, 256),
+            BottleNeck(256, 64, 256),
+            # conv2_x
+            BottleNeck(256, 128, 512, 2),
+            # size = /8
+            BottleNeck(512, 128, 512),
+            BottleNeck(512, 128, 512),
+            BottleNeck(512, 128, 512),
+            BottleNeck(512, 128, 512),
+            # conv3_x
+            BottleNeck(512, 256, 1024, 2),
+            # size = /16
+            BottleNeck(1024, 256, 1024),
+            BottleNeck(1024, 256, 1024),
+            BottleNeck(1024, 256, 1024),
+            BottleNeck(1024, 256, 1024),
+            BottleNeck(1024, 256, 1024),
+            # conv4_x
+            BottleNeck(1024, 512, 2048, 2),
+            # size = /32
+            BottleNeck(2048, 512, 2048),
+            BottleNeck(2048, 512, 2048),
+            # conv5_x
+            nn.AvgPool2d(kernel_size=8)
+        )
+        self.fc = nn.Linear(2048, num_class)
+
+    def forward(self, x):
+        x = self.conv(x)
         x = self.block(x)
         x = x.view(x.size()[0], -1)
         x = self.fc(x)
--- run_net.py
+++ run_net.py
@@ -12,57 +12,43 @@ import evaluator
 train_path = 'Dataset/train'
 test_path = 'Dataset/test'
 train_set = ImageFolder(root=train_path, transform=ToTensor())
-train_data = DataLoader(train_set, batch_size=64, shuffle=True, num_workers=3)
+train_data = DataLoader(train_set, batch_size=32, shuffle=True, num_workers=3)
 test_set = ImageFolder(root=test_path, transform=ToTensor())
 test_data = DataLoader(test_set, batch_size=32, shuffle=False, num_workers=3)
 # Load dataset.
 
-model = resnet.ResNet34(45)
+# model = resnet.ResNet34(45)
+model = resnet.ResNet50(45)
 if torch.cuda.is_available():
     model.cuda()
 # Initialize CNN.
 
-max_iteration = 20
+max_iteration = 10
 lr = 1e-3
-weight_decay = 1e-4
 # Define hyper-parameter
 
 total_train_acc = np.zeros(max_iteration)
 total_test_acc = np.zeros(max_iteration)
 total_train_loss = np.zeros(max_iteration)
 total_test_loss = np.zeros(max_iteration)
+lr_record = np.zeros(max_iteration)
 
 if __name__ == '__main__':
     for epoch in range(max_iteration):
         total_train_acc[epoch], total_train_loss[epoch] = trainer.train_net(model, train_data, epoch, lr=lr,
-                                                                            momentum=0.9, weight_decay=weight_decay)
+                                                                            momentum=0.9, weight_decay=1e-4)
         torch.cuda.empty_cache()
         total_test_acc[epoch], total_test_loss[epoch] = evaluator.test_net(model, test_data, epoch)
         torch.cuda.empty_cache()
+        lr_record[epoch] = lr
 
-        if epoch == 4 or epoch == 8 or epoch == 12 or epoch == 16:
+        if epoch % 3 == 2:
             lr /= 2
-            print('Learning rate changed.')
-        # Periodically change learning rate.
-        if epoch >= 4 and total_test_loss[epoch] > total_test_loss[epoch - 1] > total_test_loss[epoch - 2]:
-            if weight_decay <= 1e-3:
-                weight_decay += 1e-4
-                print('Weight decay changed.')
-                continue
-            else:
-                print('Weight decay is too big to be changed.')
-        if epoch >= 4 and total_train_acc[epoch] - total_test_acc[epoch] >= 0.1 * (1e-4 / weight_decay):
-            if weight_decay <= 5 * 1e-4:
-                weight_decay += 1e-4
-                print('Weight decay changed.')
-            else:
-                print('Weight decay is too big to be changed.')
-        # Roughly detect if over-fitting occurred.
 
     torch.save(model, 'model.pth')
 
     epoch = np.linspace(1, max_iteration, max_iteration)
-    new_tick = np.linspace(0, 20, 11)
+    new_tick = np.linspace(0, max_iteration, max_iteration + 1)
     plt.figure()
     plt.plot(epoch, total_train_acc, '-.^')
     plt.plot(epoch, total_test_acc, '-.^')
@@ -74,10 +60,9 @@ if __name__ == '__main__':
     plt.savefig('fig1.jpg')
     plt.show()
 
-    width = 0.25
     plt.figure()
-    plt.bar(epoch - width / 2, total_train_loss, width)
-    plt.bar(epoch + width / 2, total_test_loss, width)
+    plt.plot(epoch, total_train_loss, '-.^')
+    plt.plot(epoch, total_test_loss, '-.^')
     plt.title('Train & Test Loss')
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
