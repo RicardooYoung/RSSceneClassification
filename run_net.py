56d95cd8b49d5fde947054fb855adc29cefd8519
Reric Young <Reric@outlook.com>
2022-03-31 14:26:02 +0800

    small changes

--- run_net.py
+++ run_net.py
@@ -15,14 +15,14 @@ batch_size = 96
 train_path = 'Dataset/train'
 test_path = 'Dataset/test'
 train_set = ImageFolder(root=train_path, transform=ToTensor())
-train_data = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=3)
+train_data = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=3, pin_memory=True)
 test_set = ImageFolder(root=test_path, transform=ToTensor())
-test_data = DataLoader(test_set, batch_size=int(batch_size / 2), shuffle=False, num_workers=3)
+test_data = DataLoader(test_set, batch_size=int(batch_size / 2), shuffle=False, num_workers=3, pin_memory=True)
 # Load dataset.
 
-model = resnet.ResNet34(45)
+# model = resnet.ResNet34(45)
 # model = resnet.ResNet50(45)
-# model = resnet.PreResNet34(45)
+model = resnet.PreResNet34(45)
 # model = torch.load('model.pth')
 if torch.cuda.is_available():
     model.cuda()
@@ -33,6 +33,8 @@ lr = 1e-3
 momentum = 0.9
 weight_decay = 1e-4
 # Define hyper-parameter
+
+early_stop = 0
 
 total_train_acc = np.zeros(max_iteration)
 total_test_acc = np.zeros(max_iteration)
@@ -47,23 +49,33 @@ if __name__ == '__main__':
         torch.cuda.empty_cache()
         total_test_acc[epoch], total_test_loss[epoch] = evaluator.test_net(model, test_data, epoch)
         torch.cuda.empty_cache()
-        print(weight_decay)
 
-        if epoch > 5:
+        if epoch > 19:
+            temp = total_train_acc[epoch - 5:epoch + 1]
+            max_acc = max(temp)
+            min_acc = min(temp)
+            if max_acc - min_acc < 1e-4:
+                early_stop = epoch + 1
+                break
+
+        if epoch > 9:
             temp = total_train_acc[epoch - 2:epoch + 1]
             max_acc = max(temp)
             min_acc = min(temp)
-            if max_acc - min_acc < 0.02:
+            if max_acc - min_acc < 0.05:
                 lr /= 10
                 print('Learning rate changed.')
 
     torch.save(model, 'model.pth')
 
+    if early_stop != 0:
+        max_iteration = early_stop
+
     epoch = np.linspace(1, max_iteration, max_iteration)
     new_tick = np.linspace(0, max_iteration, max_iteration + 1)
     plt.figure()
-    plt.plot(epoch, total_train_acc, '-.^')
-    plt.plot(epoch, total_test_acc, '-.^')
+    plt.plot(epoch, total_train_acc[0:max_iteration], '-.^')
+    plt.plot(epoch, total_test_acc[0:max_iteration], '-.^')
     plt.title('Train & Test Accuracy')
     plt.xlabel('Epoch')
     plt.ylabel('Accuracy')
